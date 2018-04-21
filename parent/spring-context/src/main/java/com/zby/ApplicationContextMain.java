640defb143410ce3f0c613bb64cd61079368db75
baiyunzhou <1181905571@qq.com>
2018-04-21 16:18:37 +0800

    运行demo

--- parent/spring-context/src/main/java/com/zby/ApplicationContextMain.java
+++ parent/spring-context/src/main/java/com/zby/ApplicationContextMain.java
@@ -1,18 +1,17 @@
 package com.zby;
 
-
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.zby.bean.CommonBean;
 
-public class ApplicationContextMain { 
+public class ApplicationContextMain {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-		CommonBean commonBean=(CommonBean)applicationContext.getBean("commonBean");
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"classpath:applicationContext.xml");
+		CommonBean commonBean = (CommonBean) applicationContext.getBean("commonBean");
 		commonBean.sayHello();
-		applicationContext.getClass().getDeclaredMethod("close").invoke(applicationContext);
+		applicationContext.close();
 	}
 
 }
